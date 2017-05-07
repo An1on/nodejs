e63614274eeac7c204908b58ece563124cb9e0be
An1on <anton.ts1994@gmail.com>
2017-05-07 17:00:16 +0300

    deleted node_modules

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-/**
- ЗАДАЧА
- Написать HTTP-сервер для загрузки и получения файлов
- - Все файлы находятся в директории files
- - Структура файлов НЕ вложенная.
+const Koa = require('koa');
+const Router = require('koa-router');
 
- - Виды запросов к серверу
-   GET /file.ext
-   - выдаёт файл file.ext из директории files,
+const app = new Koa();
+const router = new Router();
 
-   POST /file.ext
-   - пишет всё тело запроса в файл files/file.ext и выдаёт ОК
-   - если файл уже есть, то выдаёт ошибку 409
-   - при превышении файлом размера 1MB выдаёт ошибку 413
+router.get('/', async function(ctx, next) {
+  ctx.body = 'Hello Koa!';
+  next();
+});
 
-   DELETE /file
-   - удаляет файл
-   - выводит 200 OK
-   - если файла нет, то ошибка 404
-
- Вместо file может быть любое имя файла.
- Так как поддиректорий нет, то при наличии / или .. в пути сервер должен выдавать ошибку 400.
-
-- Сервер должен корректно обрабатывать ошибки "файл не найден" и другие (ошибка чтения файла)
-- index.html или curl для тестирования
-
- */
-
-// отдачу файлов следует переделать "правильно", через потоки, с нормальной обработкой ошибок
-/*        fs.readFile(ROOT + 'index.html', (err, content) => {
-          if (err) throw err;
-          res.setHeader('Content-Type', 'text/html;charset=utf-8');
-          res.end(content);
-        });*/
-
-// Пример простого сервера в качестве основы
-
-'use strict';
-
-const a = 1;
-const b = 2;
-
-let f = () => a + b;
-f();
-
-// const url = require('url');
-// const fs = require('fs');
-// const {
-//   Server
-// } = require('http');
-// const ROOT = __dirname + '/public/'
-// const log = console.log;
-// const mime = require('./../lib/node_modules/mime');
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000);
---
+++ test/tests.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+/* eslint-disable*/
+const mocha = require('mocha');
+/* eslint-enable*/
+const f = () => 1;
+/* eslint-disable no-undef */
+describe('First test once function', function() {
+  describe('use f', function() {
+    it('f should be a function', function() {
+      expect(f).to.be.a('function');
+    });
+
+    it('f should be a function', function() {
+      expect(f).to.be.a('function');
+    });
+  });
+});
+/* eslint-disable*/
+function findAll(n, k) {
+  const arr = [...Array(k)].map(num => {
+
+  });
+  arr.sort((a, b) => +(a > b) || -1)
+  return [];
+}
